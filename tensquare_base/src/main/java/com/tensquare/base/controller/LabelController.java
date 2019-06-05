553622db52a10222ce4c529bb61bffe6e829b4c4
wangates <mickeyking98@126.com>
2019-06-05 22:33:32 +0800

    创建service包，包下创建LabelService类。 在这个类中，实现基本的增删改查功能。  

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -1,40 +1,47 @@
 package com.tensquare.base.controller;
 
 import com.tensquare.base.pojo.Label;
+import com.tensquare.base.service.LabelService;
 import entity.Result;
 import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/label")
 public class LabelController {
-
+    @Autowired
+    private LabelService labelService;
     //查询全部列表
     @RequestMapping(method = RequestMethod.GET)
     public Result findAll(){
-        return new Result(true, StatusCode.OK,"查询成功");
+        return new Result(true, StatusCode.OK,"查询成功", labelService.findAll());
     }
 
     //根据ID查询
     @RequestMapping(value = "/{labelId}", method = RequestMethod.GET)
     public Result findById(@PathVariable("labelId") String labelId){
-        return new Result(true, StatusCode.OK,"查询成功");
+        return new Result(true, StatusCode.OK,"查询成功", labelService.findById(labelId));
     }
 
     @RequestMapping(method = RequestMethod.POST)
     //把页面的json转成label对象
     public Result save(@RequestBody Label label){
+        labelService.save(label);
         return new Result(true, StatusCode.OK, "添加成功");
     }
 
     @RequestMapping(value = "/{labelId}", method = RequestMethod.PUT)
     public Result update (@PathVariable String labelId, @RequestBody Label label){
+        label.setId(labelId);
+        labelService.update(label);
         return new Result(true, StatusCode.OK, "更新成功");
     }
 
     @RequestMapping(value = "/{labelId}", method = RequestMethod.DELETE)
     public Result deleteById (@PathVariable String labelId){
+        labelService.deleteById(labelId);
         return new Result(true, StatusCode.OK, "删除成功");
     }
 
---
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -0,0 +1,41 @@
+package com.tensquare.base.service;
+
+import com.tensquare.base.dao.LabelDao;
+import com.tensquare.base.pojo.Label;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import util.IdWorker;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+
+public class LabelService {
+    @Autowired
+    private LabelDao labelDao;
+
+    @Autowired
+    private IdWorker idWorker;
+
+    public List<Label> findAll(){
+        return labelDao.findAll();
+    }
+    public Label findById(String id){
+        return labelDao.findById(id).get();
+    }
+
+    public void save(Label label){
+        label.setId(idWorker.nextId()+"");
+        labelDao.save(label);
+    }
+
+    public void update(Label label){
+        labelDao.save(label);
+    }
+
+     public void deleteById(String id){
+        labelDao.deleteById(id);
+     }
+}
