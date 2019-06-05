cb10c49e1c03e03b832fbcbc5c35e8c9677b8988
wangates <mickeyking98@126.com>
2019-06-05 10:41:28 +0800

    用ResponseBody将对象转成前端所需要的json格式

--- tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -3,17 +3,17 @@ package com.tensquare.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import util.IdWorker;
 
 @SpringBootApplication
+
 public class BaseApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BaseApplication.class, args );//springboot项目的入口
+        SpringApplication.run(BaseApplication.class, args);
     }
 
     @Bean//纯注解
     public IdWorker idWorker(){
-        return new IdWorker(1,2);
+        return new IdWorker(1,1);
     }
 }
---
+++ tensquare_base/src/main/java/com/tensquare/base/controller/BaseController.java
@@ -0,0 +1,7 @@
+package com.tensquare.base.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+}
