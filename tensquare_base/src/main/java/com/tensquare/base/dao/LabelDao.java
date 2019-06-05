9c4ec6daf3951ab8481893ee49328e9622b8ec31
wangates <mickeyking98@126.com>
2019-06-05 22:08:26 +0800

    创建LabelDao接口

--- tensquare_base/src/main/java/com/tensquare/base/dao/LabelDao.java
+++ tensquare_base/src/main/java/com/tensquare/base/dao/LabelDao.java
@@ -1,4 +1,8 @@
 package com.tensquare.base.dao;
 
-public interface LabelDao {
+import com.tensquare.base.pojo.Label;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface LabelDao extends JpaRepository<Label, String>, JpaSpecificationExecutor<Label> {
 }
